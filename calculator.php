de0cc2d0142cb56bea4758f6b988a890e7b0f550
Joao G <joao.guylherme@hotmail.com>
2023-06-01 14:24:24 -0300

    adding user input

---
+++ calculator.php
@@ -0,0 +1,7 @@
+<?php
+
+$firstNum = readline("What is the first number of the operation? ");
+$secNum = readline("What is the second number of the operation? ");
+$operator = readline("What is the operation sign (+, -, *, /)? ");
+
+?>
