f5203a438ea8de396b7af9e6c0304314ff89dc5f
Joao G <joao.guylherme@hotmail.com>
2023-06-01 14:29:24 -0300

    added sum operation

--- calculator.php
+++ calculator.php
@@ -3,5 +3,25 @@
 $firstNum = readline("What is the first number of the operation? ");
 $secNum = readline("What is the second number of the operation? ");
 $operator = readline("What is the operation sign (+, -, *, /)? ");
+$result = 0;
+
+switch ($operator) {
+    case '+':
+        $result = $firstNum + $secNum;
+        break;
+    case '-':
+        # code...
+        break;
+    case '*':
+        # code...
+        break;
+    case '/':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
 
 ?>
